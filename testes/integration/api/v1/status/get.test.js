4becd7a4d10000f6724110e0d63d869a2d8157c5
Vinicius Ribeiro <157416457+Vineco77@users.noreply.github.com>
2025-12-21 22:58:49 +0000

    feat(database): Configure PostgreSQL connection and add .env file on pourpose

--- testes/integration/api/v1/status/get.test.js
+++ testes/integration/api/v1/status/get.test.js
@@ -1,5 +1,11 @@
-it("GET to /api/v1/status should return 200", async () => {
+it("GET to /api/v1/status should return 200 on prod", async () => {
   const response = await fetch("https://mendoncadog.com.br");
+
+  expect(response.status).toBe(200);
+});
+
+it("GET to /api/v1/status should return 200 on dev", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
 });
