c2db42df9967275588e475f3f248d9d794c992fe
Vinicius Ribeiro <157416457+Vineco77@users.noreply.github.com>
2026-06-13 15:29:34 +0000

    test(rename all descriptions to use new patten): rename all description to use new patten

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,25 +4,29 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-it("GET to /api/v1/status should return 200 on dev", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    it("Retrievinf current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+      const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
-  if (process.env.NODE_ENV === "development") {
-    expect(responseBody.dependencies.database.version).toEqual("16.0");
-    expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  } else {
-    expect(responseBody.dependencies.database.version).toEqual(
-      "16.11 (74c6bb6)",
-    );
-    expect(responseBody.dependencies.database.max_connections).toEqual(901);
-  }
+      if (process.env.NODE_ENV === "development") {
+        expect(responseBody.dependencies.database.version).toEqual("16.0");
+        expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      } else {
+        expect(responseBody.dependencies.database.version).toEqual(
+          "16.11 (74c6bb6)",
+        );
+        expect(responseBody.dependencies.database.max_connections).toEqual(901);
+      }
 
-  expect(responseBody.dependencies.database.open_connections).toEqual(1);
+      expect(responseBody.dependencies.database.open_connections).toEqual(1);
+    });
+  });
 });
